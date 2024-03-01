93b30007b85b3e7307178fbdf0ced10c4824c7d7
waditya <waditya02@gmail.com>
2024-02-29 23:44:30 -0600

    Update About component with User component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+import User from "./User";
+
 const About = () => {
     return (
         <div>
             <h1>About Us</h1>
             <h2> This company was formed in February 2024</h2>
+            <h2> Meet the team </h2>
+            <div>
+                <User />    
+            </div>
+            
         </div>
     )
 }
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = () => {
+    return (
+        <div className = "user-card">
+            <h2>Name : Aditya W</h2>
+            <h3>Location : United States</h3>
+            <h4>Contact : email@email.com</h4>
+        </div>
+    )
+}
+
+export default User;
