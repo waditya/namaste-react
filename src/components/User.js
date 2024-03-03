be71094fd4eeed8cdd83f07f495279fad8ab7fdd
waditya <waditya02@gmail.com>
2024-03-03 00:20:48 -0600

    Add multiple state variables

--- src/components/User.js
+++ src/components/User.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const User = (props) => {
     const [count] = useState(0);
+    const [count2] = useState(2);
     return (
         <div className = "user-card">
             <h1>Count = {count} </h1>
+            <h1>Count2 = {count2} </h1>
             <h2>Name : {props.name}</h2>
             <h3>Location : {props.location}</h3>
             <h4>Contact : {props.contact}</h4>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,15 +5,17 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         console.log(props);
-
+        // this.state is a big object which consists of all the state variable
         this.state = {
             count:0,
+            count2:2,
         };
     }
     render() {
         return (
             <div className = "user-card">
             <h1>Count : {this.state.count}</h1>
+            <h1>Count2 : {this.state.count2}</h1>
             <h2>Name :{ this.props.name}</h2>
             <h3>Location :  {this.props.location}</h3>
             <h4>Contact :  {this.props.contact}</h4>
